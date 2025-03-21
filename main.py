da8c2ea066c2c275b0c18a9fdf7329df1f828dc4
KNakshitha <nakshitha2955@gmail.com>
2025-03-21 12:50:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ st.write('this is a simple program')
 st.button('say hello')
 st.text('hello streamlit')
 st.text_input('Enter your name')
+
+st.radio("gender",("male","female"),index=None)
+
+if name:
+    st.write(f'hello,{name}!')
+if st.file_uploader('plz upload files:',type=['txt','csv']):
+    st.write("thanks for uploading")
